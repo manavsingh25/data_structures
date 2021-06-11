2d426048a377dd0641d0a4330eb4ba0e94047a22
manav <manav.singh.549668@gmail.com>
2021-06-11 22:20:10 +0530

    hacker rank

---
+++ src/HackerRank/experiment.java
@@ -0,0 +1,40 @@
+package HackerRank;
+
+public class experiment {
+    public static void main(String[] args) {
+        int arr[]={0,0,0,0,0,0,0,0,0,0};
+        for (int z = 0; z < arr.length; z++) {
+            System.out.print(arr[z] + " ");
+        }
+        System.out.println();
+        for(int i=0;i<arr.length;i++) {
+            if (i >= 0 && i < 5) {
+                arr[i] = arr[i] + 3;
+            }
+        }
+        for (int z = 0; z < arr.length; z++) {
+            System.out.print(arr[z] + " ");
+        }
+        System.out.println();
+        for(int i=0;i<arr.length;i++) {
+            if (i >= 3 && i < 8) {
+                arr[i] = arr[i] + 7;
+            }
+        }
+        for (int z = 0; z < arr.length; z++) {
+            System.out.print(arr[z] + " ");
+        }
+        System.out.println();
+        for(int i=0;i<arr.length;i++) {
+            if(i>=5&&i<9)
+            {
+                arr[i]=arr[i]+1;
+            }
+        }
+        for(int z=0;z<arr.length;z++)
+        {
+            System.out.print(arr[z]+" ");
+        }
+        System.out.println();
+    }
+}
